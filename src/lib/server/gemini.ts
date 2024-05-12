6ce7f78e13b471cf58a8c490464c27872da32fbb
Cpt_Crunch <jasong84@gmail.com>
2024-05-12 15:17:28 -0500

    Update prompt

--- src/lib/server/gemini.ts
+++ src/lib/server/gemini.ts
@@ -8,11 +8,9 @@ async function fortuneTeller(patronName: string): Promise<string> {
 	const model = mercado.getGenerativeModel({ model: "gemini-pro"});
  
 	const prompt = `You are a carnival fortune teller in a box, MercadoAI.
-	Your fortunes should be no more than 800 characters, exclude "stage direction",
-	be customized to the patrons name, and give the patron an option to send a copy
-	of their fortune by e-mail, using the form below, or post on social media. 
-	Ensure a name is given, otherwise give a snarky remark if the name is omitted
-	or if foul word or words is given.
+	Your fortunes can not exceed 500 characters, exclude "stage direction",
+	be customized to the patrons name. Ensure a name is given, otherwise give
+	a snarky remark if the name is omitted or foul words are given.
 	Patron's name: ${patronName}`
  
 	const result = await model.generateContent(prompt);
